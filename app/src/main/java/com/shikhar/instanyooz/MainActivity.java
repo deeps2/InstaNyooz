4a08df44f02ad4f0c446d71f5121ac05a52771c3
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2017-01-17 02:39:36 +0530

    implemented layout for list of news articles

--- app/src/main/java/com/shikhar/instanyooz/MainActivity.java
+++ app/src/main/java/com/shikhar/instanyooz/MainActivity.java
@@ -3,11 +3,19 @@ package com.shikhar.instanyooz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_news);
+
+        // sample code snippet to set the text content on the ExpandableTextView
+        ExpandableTextView expTv1 = (ExpandableTextView)findViewById(R.id.expand_text_view);
+
+// IMPORTANT - call setText on the ExpandableTextView to set the text content to display
+        expTv1.setText("hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog. hello. a quick brown fox jumps over a lazy dog.");
     }
 }
